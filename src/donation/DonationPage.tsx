fd61ac7495b15cbe0989dc1ff2b7a8a6b7a85ef1
Jared Martin <jared@livechurchsolutions.org>
2021-08-24 17:17:16 -0500

    Add donation styling

--- src/donation/DonationPage.tsx
+++ src/donation/DonationPage.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import { PersonHelper } from "../helpers";
 import { DonationPage } from "../appBase/donationComponents/DonationPage";
 
-export const DonationPages = () => {
-
-  return (
-    <div style={{ height: "100vh", overflowY: "scroll", backgroundColor: "#F9F9F9", padding: 20 }}>
-      { PersonHelper.person?.id ? <DonationPage personId={PersonHelper.person.id} /> : <h3>Please login to make a donation.</h3> }
-    </div>);
-}
+export const DonationPages = () => (
+  <div style={{ height: "100vh", backgroundColor: "#F9F9F9", overflowY: "scroll" }}>
+    { PersonHelper.person?.id ? <DonationPage personId={PersonHelper.person.id} /> : <h3>Please login to make a donation.</h3> }
+  </div>)
