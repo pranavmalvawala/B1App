404cf9ffb38cc2f5c3fa1f9b8f32de305d71ec00
Jeremy Zongker <jeremy@trilitech.com>
2021-08-27 19:17:12 +0000

    Added Google Analytics

--- src/ControlPanel.tsx
+++ src/ControlPanel.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import UserContext from "./UserContext"
 import { Authenticated } from "./Authenticated"
 import { Unauthenticated } from "./Unauthenticated"
 import { ApiHelper } from "./components";
 import { Logout } from "./Logout";
 import { Login } from "./Login";
+import { EnvironmentHelper } from "./helpers"
+import ReactGA from "react-ga";
 
 interface Props {
-    path?: string;
+  path?: string;
 }
 
 export const ControlPanel = () => {
+  const location = useLocation();
+  if (EnvironmentHelper.GoogleAnalyticsTag !== "") {
+    ReactGA.initialize(EnvironmentHelper.GoogleAnalyticsTag);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+  React.useEffect(() => { if (EnvironmentHelper.GoogleAnalyticsTag !== "") ReactGA.pageview(location.pathname + location.search); }, [location]);
+
   let user = React.useContext(UserContext).userName; //to force rerender on login
   if (user === null) return null;
 
