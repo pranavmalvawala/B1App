f4fa54553f40495e8da115425413fea7028d0b8d
Jeremy Zongker <jeremy@trilitech.com>
2021-08-27 19:16:58 +0000

    Added Google Analytics

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,6 +6,9 @@ import { Authenticated } from "./Authenticated";
 import UserContext from "./UserContext";
 import { LoginPage } from "./appBase/pageComponents/LoginPage";
 import { AppearanceHelper } from "./appBase/helpers/AppearanceHelper";
+import { EnvironmentHelper } from "./helpers";
+import { UserInterface, ChurchInterface } from "./appBase/interfaces";
+import ReactGA from "react-ga";
 
 export const Login: React.FC = (props: any) => {
   const [cookies] = useCookies(["jwt"]);
@@ -14,6 +17,14 @@ export const Login: React.FC = (props: any) => {
   const successCallback = async () => {
     PersonHelper.person = await ApiHelper.get(`/people/${UserHelper.currentChurch.personId}`, "MembershipApi");
     context.setUserName(UserHelper.currentChurch.id.toString());
+  }
+
+  const trackChurchRegister = async (church: ChurchInterface) => {
+    if (EnvironmentHelper.GoogleAnalyticsTag !== "") ReactGA.event({ category: "Church", action: "Register" });
+  }
+
+  const trackUserRegister = async (user: UserInterface) => {
+    if (EnvironmentHelper.GoogleAnalyticsTag !== "") ReactGA.event({ category: "User", action: "Register" });
   }
 
   const context = React.useContext(UserContext);
@@ -30,7 +41,9 @@ export const Login: React.FC = (props: any) => {
         auth={auth}
         context={context}
         jwt={jwt}
-        successCallback={successCallback}
+        loginSuccessOverride={successCallback}
+        churchRegisteredCallback={trackChurchRegister}
+        userRegisteredCallback={trackUserRegister}
         requiredKeyName={false}
         appName="B1"
         logo={AppearanceHelper.getLogoLight(ConfigHelper.current?.appearance, null)}
--- src/helpers/EnvironmentHelper.ts
+++ src/helpers/EnvironmentHelper.ts
@@ -1,59 +1,63 @@
 import { ApiHelper } from "../appBase/helpers/ApiHelper";
 
 export class EnvironmentHelper {
-    private static AccessApi = "";
-    private static B1Api = "";
-    private static MembershipApi = "";
-    private static AttendanceApi = "";
-    private static GivingApi = "";
-    static ContentRoot = "";
-    static ChurchAppsUrl = "";
+  private static AccessApi = "";
+  private static B1Api = "";
+  private static MembershipApi = "";
+  private static AttendanceApi = "";
+  private static GivingApi = "";
+  static ContentRoot = "";
+  static ChurchAppsUrl = "";
+  static GoogleAnalyticsTag = "";
 
-    static init = () => {
-      switch (process.env.REACT_APP_STAGE) {
-        case "staging": EnvironmentHelper.initStaging(); break;
-        case "prod": EnvironmentHelper.initProd(); break;
-        default: EnvironmentHelper.initDev(); break;
-      }
-      ApiHelper.apiConfigs = [
-        { keyName: "AccessApi", url: EnvironmentHelper.AccessApi, jwt: "", permisssions: [] },
-        { keyName: "AttendanceApi", url: EnvironmentHelper.AttendanceApi, jwt: "", permisssions: [] },
-        { keyName: "MembershipApi", url: EnvironmentHelper.MembershipApi, jwt: "", permisssions: [] },
-        { keyName: "B1Api", url: EnvironmentHelper.B1Api, jwt: "", permisssions: [] },
-        { keyName: "GivingApi", url: EnvironmentHelper.GivingApi, jwt: "", permisssions: [] }
-      ];
+  static init = () => {
+    switch (process.env.REACT_APP_STAGE) {
+      case "staging": EnvironmentHelper.initStaging(); break;
+      case "prod": EnvironmentHelper.initProd(); break;
+      default: EnvironmentHelper.initDev(); break;
     }
+    ApiHelper.apiConfigs = [
+      { keyName: "AccessApi", url: EnvironmentHelper.AccessApi, jwt: "", permisssions: [] },
+      { keyName: "AttendanceApi", url: EnvironmentHelper.AttendanceApi, jwt: "", permisssions: [] },
+      { keyName: "MembershipApi", url: EnvironmentHelper.MembershipApi, jwt: "", permisssions: [] },
+      { keyName: "B1Api", url: EnvironmentHelper.B1Api, jwt: "", permisssions: [] },
+      { keyName: "GivingApi", url: EnvironmentHelper.GivingApi, jwt: "", permisssions: [] }
+    ];
+  }
 
-    static initDev = () => {
-      EnvironmentHelper.AccessApi = process.env.REACT_APP_ACCESS_API || "";
-      EnvironmentHelper.MembershipApi = process.env.REACT_APP_MEMBERSHIP_API || "";
-      EnvironmentHelper.AttendanceApi = process.env.REACT_APP_ATTENDANCE_API || "";
-      EnvironmentHelper.B1Api = process.env.REACT_APP_B1_API || "";
-      EnvironmentHelper.GivingApi = process.env.REACT_APP_GIVING_API || "";
-      EnvironmentHelper.ContentRoot = process.env.REACT_APP_CONTENT_ROOT || "";
-      EnvironmentHelper.ChurchAppsUrl = process.env.REACT_APP_CHURCH_APPS_URL || "";
-    }
+  static initDev = () => {
+    EnvironmentHelper.AccessApi = process.env.REACT_APP_ACCESS_API || "";
+    EnvironmentHelper.MembershipApi = process.env.REACT_APP_MEMBERSHIP_API || "";
+    EnvironmentHelper.AttendanceApi = process.env.REACT_APP_ATTENDANCE_API || "";
+    EnvironmentHelper.B1Api = process.env.REACT_APP_B1_API || "";
+    EnvironmentHelper.GivingApi = process.env.REACT_APP_GIVING_API || "";
+    EnvironmentHelper.ContentRoot = process.env.REACT_APP_CONTENT_ROOT || "";
+    EnvironmentHelper.ChurchAppsUrl = process.env.REACT_APP_CHURCH_APPS_URL || "";
+    EnvironmentHelper.GoogleAnalyticsTag = process.env.REACT_APP_GOOGLE_ANALYTICS_TAG || "";
+  }
 
-    //NOTE: None of these values are secret.
-    static initStaging = () => {
-      EnvironmentHelper.AccessApi = "https://accessapi.staging.churchapps.org";
-      EnvironmentHelper.AttendanceApi = "https://attendanceapi.staging.churchapps.org";
-      EnvironmentHelper.MembershipApi = "https://membershipapi.staging.churchapps.org";
-      EnvironmentHelper.B1Api = "https://api.staging.b1.church";
-      EnvironmentHelper.GivingApi ="https://givingapi.staging.churchapps.org";
-      EnvironmentHelper.ContentRoot = "https://content.staging.churchapps.org";
-      EnvironmentHelper.ChurchAppsUrl = "https://staging.churchapps.org";
-    }
+  //NOTE: None of these values are secret.
+  static initStaging = () => {
+    EnvironmentHelper.AccessApi = "https://accessapi.staging.churchapps.org";
+    EnvironmentHelper.AttendanceApi = "https://attendanceapi.staging.churchapps.org";
+    EnvironmentHelper.MembershipApi = "https://membershipapi.staging.churchapps.org";
+    EnvironmentHelper.B1Api = "https://api.staging.b1.church";
+    EnvironmentHelper.GivingApi = "https://givingapi.staging.churchapps.org";
+    EnvironmentHelper.ContentRoot = "https://content.staging.churchapps.org";
+    EnvironmentHelper.ChurchAppsUrl = "https://staging.churchapps.org";
+    EnvironmentHelper.GoogleAnalyticsTag = "";
+  }
 
-    //NOTE: None of these values are secret.
-    static initProd = () => {
-      EnvironmentHelper.AccessApi = "https://accessapi.churchapps.org";
-      EnvironmentHelper.AttendanceApi = "https://attendanceapi.churchapps.org";
-      EnvironmentHelper.MembershipApi = "https://membershipapi.churchapps.org";
-      EnvironmentHelper.B1Api = "https://api.b1.church";
-      EnvironmentHelper.GivingApi = "https://givingapi.churchapps.org";
-      EnvironmentHelper.ContentRoot = "https://content.churchapps.org";
-      EnvironmentHelper.ChurchAppsUrl = "https://churchapps.org"
-    }
+  //NOTE: None of these values are secret.
+  static initProd = () => {
+    EnvironmentHelper.AccessApi = "https://accessapi.churchapps.org";
+    EnvironmentHelper.AttendanceApi = "https://attendanceapi.churchapps.org";
+    EnvironmentHelper.MembershipApi = "https://membershipapi.churchapps.org";
+    EnvironmentHelper.B1Api = "https://api.b1.church";
+    EnvironmentHelper.GivingApi = "https://givingapi.churchapps.org";
+    EnvironmentHelper.ContentRoot = "https://content.churchapps.org";
+    EnvironmentHelper.ChurchAppsUrl = "https://churchapps.org";
+    EnvironmentHelper.GoogleAnalyticsTag = "UA-164774603-8";
+  }
 
 }
