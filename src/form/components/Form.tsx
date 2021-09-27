b139725ec902bc9dae71989995012554150ed7cf
Jared Martin <jared@livechurchsolutions.org>
2021-09-27 12:42:21 -0500

    Update form page

--- src/form/components/Form.tsx
+++ src/form/components/Form.tsx
@@ -1,48 +1,40 @@
 import React from "react";
-import { PersonHelper } from "../../helpers";
+import { PersonHelper, ConfigHelper } from "../../helpers";
 import { ApiHelper } from "../../components";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { FormSubmissionEdit, Loading } from "../../appBase/components";
 
 interface Props { formId: string }
 
 export const Form: React.FC<Props> = (props) => {
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [restrictedForm, setRestrictedForm] = React.useState<boolean>(null);
-  const [member, setMember] = React.useState<any>(null);
+  const [restrictedForm, setRestrictedForm] = React.useState<boolean>(true);
   const [formSubmitted, setFormSubmitted] = React.useState<boolean>(null);
+  const [addFormId, setAddFormId] = React.useState<string>("");
+  const [unRestrictedFormId, setUnRestrictedFormId] = React.useState<string>("");
+  const [redirectTo, setRedirectTo] = React.useState<string>("");
 
   const loadData = () => {
-    ApiHelper.getAnonymous("/forms/access/" + props.formId, "MembershipApi").then(result => {
-      setRestrictedForm(result.restricted);
-      if (result.restricted) {
-        ApiHelper.get("/memberpermissions/form/" + props.formId, "MembershipApi").then(data => {
-          setMember(data[0]);
-          setLoading(false);
-        });
-      } else setLoading(false);
+    setLoading(true);
+    ApiHelper.get("/forms/standalone/" + props.formId + "?churchId=" + ConfigHelper.churchId, "MembershipApi").then(data => {
+      setRestrictedForm(data.restricted);
+      if (data.restricted) setAddFormId(props.formId);
+      else setUnRestrictedFormId(props.formId);
+      setLoading(false);
     });
   }
 
   const handleUpdate = () => { setFormSubmitted(true); }
-
-  const showForm = () => <FormSubmissionEdit addFormId={props.formId} contentType="form" contentId={props.formId} formSubmissionId="" updatedFunction={handleUpdate} />
-
-  const isLoading = loading || (PersonHelper?.person?.id && member === null);
-
+  const showForm = () => <FormSubmissionEdit churchId={ConfigHelper.churchId} addFormId={addFormId} unRestrictedFormId={unRestrictedFormId} contentType="form" contentId={props.formId} formSubmissionId="" personId={PersonHelper?.person?.id} updatedFunction={handleUpdate} cancelFunction={ () => setRedirectTo("/")} />
   const getForm = () => {
-    if (isLoading) return <Loading />;
+    if (loading) return <Loading />;
     if (!restrictedForm) return showForm();
-    if (!PersonHelper?.person?.id) return <h3 style={{textAlign: "center"}}><Link to={"/login?returnUrl=/forms/" + props.formId}>Login</Link> to view this form.</h3>;
-    if (!member?.id) return <h3>You are not authorized to view this form.</h3>;
-    return formSubmitted ? <h1>Form submitted</h1> : showForm();
+    if (!PersonHelper?.person?.id) return <h3 className="text-center"><Link to={"/login?returnUrl=/forms/" + props.formId}>Login</Link> to view this form.</h3>;
+    return formSubmitted ? <h3 className="text-center">Your form has been successfully submitted. <Link to={"/"}>Return Home</Link></h3> : showForm();
   }
 
   React.useEffect(() => loadData(), [loadData, props.formId]);
 
-  return (
-    <>
-      { getForm() }
-    </>
-  );
+  if (redirectTo) return <Redirect to={redirectTo} />;
+  else return (<>{ getForm() }</>);
 }
