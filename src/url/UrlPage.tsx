5d030be601be74fb1848b6a4ffc96af519730a27
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-10-02 10:50:15 +0530

    make iframe tabs full screen (#29)

--- src/url/UrlPage.tsx
+++ src/url/UrlPage.tsx
@@ -1,13 +1,10 @@
 import * as React from "react"
-import { Container } from "react-bootstrap"
 import { useQuery } from "../hooks/useQuery"
 
 export function UrlPage() {
   const query = useQuery()
 
   return (
-    <Container>
-      <iframe title="content" className="w-100 vh-85" src={query.get("link")} />
-    </Container>
+    <iframe title="content" className="full-frame" src={query.get("link")} />
   )
 }
