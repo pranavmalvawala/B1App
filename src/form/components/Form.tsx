7d7af5b726deebd7ab0991b53e1d367dd82bb113
Jared Martin <jared@livechurchsolutions.org>
2021-10-20 22:28:20 -0500

    Stand alone form updates

--- src/form/components/Form.tsx
+++ src/form/components/Form.tsx
@@ -3,6 +3,7 @@ import { PersonHelper, ConfigHelper } from "../../helpers";
 import { ApiHelper } from "../../components";
 import { Link, Redirect } from "react-router-dom";
 import { FormSubmissionEdit, Loading } from "../../appBase/components";
+import { DateHelper } from "../../appBase/helpers";
 
 interface Props { formId: string }
 
@@ -13,10 +14,17 @@ export const Form: React.FC<Props> = (props) => {
   const [addFormId, setAddFormId] = React.useState<string>("");
   const [unRestrictedFormId, setUnRestrictedFormId] = React.useState<string>("");
   const [redirectTo, setRedirectTo] = React.useState<string>("");
+  const [early, setEarly] = React.useState<Date>(null);
+  const [late, setLate] = React.useState<Date>(null);
 
   const loadData = () => {
     setLoading(true);
     ApiHelper.get("/forms/standalone/" + props.formId + "?churchId=" + ConfigHelper.churchId, "MembershipApi").then(data => {
+      let now = new Date().setHours(0,0,0,0);
+      let start = data.accessStartTime ? new Date(data.accessStartTime) : null;
+      let end = data.accessEndTime ? new Date(data.accessEndTime) : null;
+      if (start && start.setHours(0,0,0,0) > now) setEarly(start);
+      if (end && end.setHours(0,0,0,0) < now) setLate(end);
       setRestrictedForm(data.restricted);
       if (data.restricted) setAddFormId(props.formId);
       else setUnRestrictedFormId(props.formId);
@@ -24,17 +32,21 @@ export const Form: React.FC<Props> = (props) => {
     });
   }
 
-  const handleUpdate = () => { setFormSubmitted(true); }
+  const handleUpdate = () => setFormSubmitted(true);
   const showForm = () => <FormSubmissionEdit churchId={ConfigHelper.churchId} addFormId={addFormId} unRestrictedFormId={unRestrictedFormId} contentType="form" contentId={props.formId} formSubmissionId="" personId={PersonHelper?.person?.id} updatedFunction={handleUpdate} cancelFunction={ () => setRedirectTo("/")} />
   const getForm = () => {
     if (loading) return <Loading />;
-    if (!restrictedForm) return showForm();
+    if (early) return <h3 className="text-center">This form isn't available until {DateHelper.prettyDateTime(early)}</h3>
+    if (late) return <h3 className="text-center">This form closed on {DateHelper.prettyDateTime(late)}</h3>
+    if (!restrictedForm || PersonHelper?.person?.id) return showForm();
     if (!PersonHelper?.person?.id) return <h3 className="text-center"><Link to={"/login?returnUrl=/forms/" + props.formId}>Login</Link> to view this form.</h3>;
-    return formSubmitted ? <h3 className="text-center">Your form has been successfully submitted. <Link to={"/"}>Return Home</Link></h3> : showForm();
+    return <></>;
   }
 
   React.useEffect(() => loadData(), [loadData, props.formId]);
 
   if (redirectTo) return <Redirect to={redirectTo} />;
-  else return (<>{ getForm() }</>);
+  else return (
+    <>{ formSubmitted ? <h3 className="text-center">Your form has been successfully submitted.</h3> : getForm() }</>
+  );
 }
