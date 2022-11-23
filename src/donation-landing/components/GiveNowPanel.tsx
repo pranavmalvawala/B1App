e47aa1ad464a88e134166b82d4a74e47183c82e9
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2022-11-23 10:01:18 +0530

    improve donation (#71)

--- src/donation-landing/components/GiveNowPanel.tsx
+++ src/donation-landing/components/GiveNowPanel.tsx
@@ -6,8 +6,14 @@ import { ConfigHelper } from "../../helpers";
 export function GiveNowPanel() {
   return (
     <Box sx={{ paddingX: 5, paddingTop: 3 }}>
-      <Typography variant="h5" fontWeight="bold">My Donation</Typography>
-      <NonAuthDonation churchId={ConfigHelper.churchId} />
+      <Typography variant="h5" fontWeight="bold">
+        My Donation
+      </Typography>
+      <NonAuthDonation
+        churchId={ConfigHelper.churchId}
+        mainContainerCssProps={{ sx: { boxShadow: "none", paddingY: 3 } }}
+        showHeader={false}
+      />
     </Box>
   );
 }
