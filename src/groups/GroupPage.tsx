146a191ed6a15c428acd959e346bda16ea4b13d4
Jeremy Zongker <jeremy@trilitech.com>
2022-12-28 19:45:14 -0600

    Added photo and about to group page

--- src/groups/GroupPage.tsx
+++ src/groups/GroupPage.tsx
@@ -1,5 +1,7 @@
+import { Grid } from "@mui/material";
 import * as React from "react"
 import { Link, useParams } from "react-router-dom";
+import { MarkdownPreview } from "../appBase/components";
 import { Conversations } from "../appBase/components/notes/Conversations";
 import { GroupInterface } from "../appBase/interfaces";
 import { Loading } from "../components";
@@ -13,13 +15,26 @@ export function GroupPage() {
     ApiHelper.get("/groups/" + params.id, "MembershipApi").then(data => setGroup(data));
   }
 
+  const getHeader = () => {
+    if (group.photoUrl) return (<Grid container spacing={3}>
+      <Grid item md={4} xs={12}>
+        <img id="tabImage" src={group.photoUrl} alt={group.name} style={{ maxHeight: 300 }} />
+      </Grid>
+      <Grid item md={8} xs={12}>
+        <MarkdownPreview value={group.about} />
+      </Grid>
+    </Grid>);
+    else return <MarkdownPreview value={group.about} />
+  }
+
   React.useEffect(loadData, []); //eslint-disable-line
 
   if (!UserHelper.currentUserChurch?.person?.id) return (<><h1>Group</h1><h3 className="text-center w-100">Please <Link to="/login/?returnUrl=/directory">Login</Link> to view your groups.</h3></>)
   else if (!group) return <Loading />
-  return (
+  else return (
     <>
       <h1>{group.name}</h1>
+      {getHeader()}
       <Conversations contentType="group" contentId={group.id} groupId={group.id} />
     </>
   )
